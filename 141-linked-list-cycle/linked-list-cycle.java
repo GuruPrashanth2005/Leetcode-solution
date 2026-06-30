2d1cc9b506e99ed7caf1a95f0973817458ca9191
GURU PRASHANTH S <guruprashanths.al24@bitsathy.ac.in>
2026-06-30 13:47:54 +0530

    Time: 8 ms (9.54%) | Memory: 46.8 MB (32.86%) - LeetSync

---
+++ 141-linked-list-cycle/linked-list-cycle.java
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        ListNode temp  =head;
+        Map<ListNode, Boolean> map = new HashMap<>();
+        while(temp != null){
+            if(map.containsKey(temp)){
+                return true;
+            }
+            map.put(temp, true);
+            temp = temp.next;
+        }
+        return false;
+    }
+}
